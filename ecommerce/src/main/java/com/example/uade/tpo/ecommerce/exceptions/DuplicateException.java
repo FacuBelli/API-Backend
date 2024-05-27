7b2f24b4d0af7839ca26e5f79afcc6f90bd949df
Lucas Argerich <lucasargerichcoder@gmail.com>
2024-05-26 23:19:22 -0300

    exception DuplicateException generico

---
+++ ecommerce/src/main/java/com/example/uade/tpo/ecommerce/exceptions/DuplicateException.java
@@ -0,0 +1,11 @@
+package com.example.uade.tpo.ecommerce.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.BAD_REQUEST)
+  public class DuplicateException extends Exception {
+    public DuplicateException(String message) {
+      super(message);
+  }
+}
