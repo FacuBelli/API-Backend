dff3558f6dc4c6a5eac7c29b7e80686b35cd0de6
Lucas Argerich <lucasargerichcoder@gmail.com>
2024-06-03 15:14:55 -0300

    ThemeController put y delete

--- ecommerce/src/main/java/com/example/uade/tpo/ecommerce/controllers/ThemeController.java
+++ ecommerce/src/main/java/com/example/uade/tpo/ecommerce/controllers/ThemeController.java
@@ -6,9 +6,11 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -48,5 +50,31 @@ public class ThemeController {
     ThemeBody body = ThemeBody.builder().name(themeRequest.getName()).build();
     Theme theme = themeService.createTheme(body);
     return ResponseEntity.created(URI.create("/theme/" + theme.getId())).body(theme);
+  }
+
+  @PutMapping("/{themeId}")
+  public ResponseEntity<Theme> updateTheme(@PathVariable Long themeId, @RequestBody ThemeRequest themeRequest)
+      throws NotFoundException {
+    Optional<Theme> theme = themeService.getThemeById(themeId);
+    if (!theme.isPresent()) {
+      throw new NotFoundException("El Theme(id): " + themeId + " no existe.");
+    }
+
+    ThemeBody body = ThemeBody.builder().name(themeRequest.getName()).build();
+    Theme newTheme = themeService.updateTheme(theme.get(), body);
+
+    return ResponseEntity.ok(newTheme);
+  }
+
+  @DeleteMapping("/{themeId}")
+  public ResponseEntity<Void> deleteTheme(@PathVariable Long themeId) throws NotFoundException {
+    Optional<Theme> theme = themeService.getThemeById(themeId);
+    if (!theme.isPresent()) {
+      throw new NotFoundException("El Theme(id): " + themeId + " no existe.");
+    }
+
+    themeService.deleteTheme(theme.get());
+
+    return ResponseEntity.ok().build();
   }
 }
