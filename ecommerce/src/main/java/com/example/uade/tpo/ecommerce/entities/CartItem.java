7cf33c3f86ea864b392c6ca4ef31c743c7c8ebd2
Lucas Argerich <lucasargerichcoder@gmail.com>
2024-06-04 19:09:10 -0300

    CartItem fix

--- ecommerce/src/main/java/com/example/uade/tpo/ecommerce/entities/CartItem.java
+++ ecommerce/src/main/java/com/example/uade/tpo/ecommerce/entities/CartItem.java
@@ -2,25 +2,36 @@ package com.example.uade.tpo.ecommerce.entities;
 
 import org.hibernate.annotations.DynamicUpdate;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.ManyToOne;
 import lombok.Data;
+import lombok.ToString;
 
 @Entity
 @Data
 @DynamicUpdate
 public class CartItem {
   @Id
-  @OneToOne
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @ManyToOne
   @JoinColumn(name = "user_id", referencedColumnName = "id", nullable = false)
+  @JsonBackReference
+  @ToString.Exclude
   private User user;
 
-  @Id
-  @OneToOne
+  @ManyToOne
   @JoinColumn(name = "artwork_id", referencedColumnName = "id", nullable = false)
+  @JsonBackReference
+  @ToString.Exclude
   private Artwork artwork;
 
   @Column
